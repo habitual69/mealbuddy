c663a38c1c83143789e3fa7ca5c632def2d1d344
habitual69 <admin@vpms.xyz>
2023-10-08 22:42:42 +0530

    Added MealByCategory

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Navbar from './components/Navbar';
 import SearchBar from './components/SearchBar';
 import MealResult from './components/MealResult';
 import SuggestedMeal from './components/SuggestedMeal';
+import MealByCategory from './components/MealByCategory'; // Import the new component
 import Footer from './components/Footer';
 
 function App() {
     const [searchResults, setSearchResults] = useState(null); 
+    const [isCategoryView, setIsCategoryView] = useState(false); // State to track if the category view is active
 
     return (
         <div className="bg-gray-900 text-white min-h-screen">
@@ -14,15 +16,20 @@ function App() {
             <div className="container mx-auto">
                 <SearchBar onSearchResults={setSearchResults} />
 
-                {searchResults === null && <SuggestedMeal />}
+                {/* Button to switch to category view */}
+                <button className="mb-4 bg-orange-500 text-white p-2 ml-4" onClick={() => setIsCategoryView(!isCategoryView)}>
+                    {isCategoryView ? 'Search by Name' : 'Browse by Category'}
+                </button>
 
-                {searchResults && searchResults.length === 0 && 
+                {/* Render components based on the state */}
+                {!isCategoryView && searchResults === null && <SuggestedMeal />}
+                {!isCategoryView && searchResults && searchResults.length === 0 && 
                     <div className="text-center py-10 text-xl font-semibold">No meal found.</div>
                 }
-
-                {searchResults && searchResults.length > 0 && searchResults.map(meal => (
+                {!isCategoryView && searchResults && searchResults.length > 0 && searchResults.map(meal => (
                     <MealResult key={meal.idMeal} meal={meal} />
                 ))}
+                {isCategoryView && <MealByCategory />} 
             </div>
             <Footer />
         </div>
--- src/components/apiFunction.jsx
+++ src/components/apiFunction.jsx
@@ -10,3 +10,8 @@ export const getRandomMeals = async () => {
     return data.meals;
   };
   
+  export const getMealByCategory = async (category) => {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`);
+    const data = await response.json();
+    return data.meals;
+  };
