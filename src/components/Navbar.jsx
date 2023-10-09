e3d972038364b8731235a94952cab413625e6a0a
habitual69 <admin@vpms.xyz>
2023-10-09 21:22:06 +0530

    fixed navebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { IoFastFood } from 'react-icons/io5';
 
 function Navbar() {
   return (
-    <nav className="p-5 bg-white shadow-md bg-[#333333]">
+    <nav className="p-5 shadow-md bg-[#333333]">
       <div className="container mx-auto flex items-center">
         <a href='index.html' className="flex items-center space-x-2 text-orange-500 hover:text-orange-800 transition duration-300 ease-in-out">
           <IoFastFood className='text-6xl text-orange-500'/>
