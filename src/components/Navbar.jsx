a70e50f16e5ee6dd16d0984498e5ba9fa47318e5
habitual69 <admin@vpms.xyz>
2023-10-10 09:12:48 +0530

    fix search bar alignment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ function Navbar() {
     <nav className="p-5 shadow-md bg-[#333333]">
       <div className="container mx-auto flex items-center">
         <a href='index.html' className="flex items-center space-x-2 text-orange-500 hover:text-orange-800 transition duration-300 ease-in-out">
-          <IoFastFood className='text-6xl text-orange-500'/>
-          <span className="text-4xl font-semibold text-orange-500">MealBuddy
+          <IoFastFood className='text-6xl text-[#fcc133]'/>
+          <span className="text-4xl font-semibold text-[#fcc133]">MealBuddy
           <p className="text-sm font-semibold text-[#fcc133]">Discover | Enjoy | Repeat.</p>
           </span>
           
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -17,27 +17,28 @@ function SearchBar({ onSearchResults }) {
 
     return (
         <div className="flex items-center justify-center mt-8">
-            <div className="relative">
+            <div className="relative items-center justify-center">
                 <input
                     type="text"
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
-                    className="w-80 px-10 py-2 text-[#fcc133] bg-[#1f2430] placeholder-gray-200 rounded-full focus:outline-none focus:shadow-outline shadow-md transition"
+                    className="w-80 px-10 p-2 text-[#1f2430] bg-[#fcc133] placeholder-gray-200 rounded-full focus:outline-none focus:shadow-outline shadow-md transition border-[#1f2430] border-2"
                     placeholder="Search for meals..."
                 />
-                <button 
-                    className="absolute top-0 right-0 px-5 py-2 text-white bg-[#B3541E] rounded-full hover:bg-orange-600 focus:outline-none focus:shadow-outline shadow-md transition transform hover:scale-105" 
-                    onClick={handleSearch}>
-                    Search
-                </button>
                 <span className="absolute inset-y-0 left-0 flex items-center pl-4">
                     <svg className="w-5 h-5 text-gray-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                         <circle cx="11" cy="11" r="8"></circle>
                         <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
                     </svg>
                 </span>
+                <button 
+                    className="absolute inset-y-0 right-0 transform-translate-y-1/2 px-5 p-2 text-[#fcc133] bg-[#1f2430] rounded-full hover:bg-orange-600 focus:outline-none focus:shadow-outline shadow-md transition transform hover:scale-105" 
+                    onClick={handleSearch}>
+                    Search
+                </button>
             </div>
         </div>
+
     );
 }
 
