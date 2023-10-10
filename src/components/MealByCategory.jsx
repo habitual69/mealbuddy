d297af1fb2e225cb585004ea03bc4a69a89bbb2d
habitual69 <admin@vpms.xyz>
2023-10-10 18:30:07 +0530

    updated MealByCategory

--- src/components/MealByCategory.jsx
+++ src/components/MealByCategory.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { getMealByCategory } from './apiFunction';
+import { getMealByCategory, getMealById } from './apiFunction';
+import MealResult from './MealResult';
 
 const categories = [
   "Beef", "Vegetarian", "Chicken", "Dessert", "Lamb", "Miscellaneous",
@@ -9,6 +10,7 @@ const categories = [
 function MealByCategory() {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [meals, setMeals] = useState([]);
+  const [selectedMeal, setSelectedMeal] = useState(null);
 
   useEffect(() => {
     if (selectedCategory) {
@@ -17,6 +19,11 @@ function MealByCategory() {
       });
     }
   }, [selectedCategory]);
+
+  const handleMealClick = async (mealId) => {
+    const mealDetails = await getMealById(mealId);
+    setSelectedMeal(mealDetails);
+  };
 
   return (
     <div className="p-4">
@@ -34,15 +41,23 @@ function MealByCategory() {
         </div>
       ) : (
         <div>
-          <button className="mb-4 text-[#fcc133] bg-[#1f2430] p-2" onClick={() => setSelectedCategory(null)}>Back to Categories</button>
+          <button className="mb-4 text-[#fcc133] bg-[#1f2430] p-2" onClick={() => { setSelectedCategory(null); setSelectedMeal(null); }}>Back to Categories</button>
           <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
             {meals.map(meal => (
               <div key={meal.idMeal} className="bg-[#1f2430] rounded-lg">
                 <img src={meal.strMealThumb} alt={meal.strMeal} className="w-full h-40 object-cover mb-2 rounded-t-lg" />
-                <h3 className="font-bold text-[#fcc133] text-center">{meal.strMeal}</h3>
+                <h3 
+                    className="font-bold text-[#fcc133] text-center cursor-pointer"
+                    onClick={() => handleMealClick(meal.idMeal)}
+                >
+                    {meal.strMeal}
+                </h3>
               </div>
             ))}
           </div>
+          {selectedMeal && (
+            <MealResult meal={selectedMeal} />
+          )}
         </div>
       )}
     </div>
