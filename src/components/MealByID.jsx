32c91ef42934bb30531bb378ae0a134d4e457c37
habitual69 <admin@vpms.xyz>
2023-10-10 14:05:15 +0530

    some more UI changes

---
+++ src/components/MealByID.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MealByID() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MealByID
--- src/components/MealResult.jsx
+++ src/components/MealResult.jsx
@@ -6,28 +6,37 @@ const MealResult = ({ meal }) => {
         return <div className="flex justify-center items-center h-screen text-xl text-gray-600">No result found.</div>;
     }
     return (
-        <div className="bg-white rounded-lg shadow-lg mt-6 mb-6 text-black">
-            <img src={meal.strMealThumb} alt={meal.strMeal} className="w-full h-[500px] rounded-t-lg mb-4 object-cover" />
-            <h2 className="text-2xl font-bold mb-4 ml-10 mr-20">{meal.strMeal}</h2>
-            <p className="font-semibold text-orange-500 ml-10 mr-20">Category: {meal.strCategory} | Cuisine: {meal.strArea}</p>
-            <p className="mt-2 mb-4 text-gray-700 italic ml-10 mr-20">Tags: {meal.strTags}</p>
-            <p className="mt-4 mb-2 font-semibold text-lg ml-10 mr-20">Instructions:</p>
-            <p className="text-gray-600 ml-10 mr-20">{meal.strInstructions}</p>
-            <p className="mt-4 mb-2 font-semibold text-lg ml-10 mr-20">Ingredients:</p>
-            <ul className="list-disc list-inside text-gray-600 ml-10 mr-20 mb-5">
-                {[...Array(20).keys()].map(i => {
-                    const ingredient = meal[`strIngredient${i + 1}`];
-                    const measure = meal[`strMeasure${i + 1}`];
-                    if (ingredient && ingredient.trim() !== "") {
-                        return (
-                            <li key={i} className="mb-1">
-                                {ingredient}: {measure}
-                            </li>
-                        );
-                    }
-                    return null;  // Return null to satisfy React's map requirements
-                })}
-            </ul>
+        <div className="bg-[#1f2430] rounded-lg shadow-lg mt-6 mb-6 text-[#fcc133] p-10">
+            <img src={meal.strMealThumb} alt={meal.strMeal} className="mt-2 w-full sm:h-[300px] mb-4 object-cover mx-auto rounded-xl" />
+            <h2 className="text-2xl font-bold mb-4 mr-20">{meal.strMeal}</h2>
+            <p className="font-semibold text-orange-500 mr-20">Category: {meal.strCategory} | Cuisine: {meal.strArea}</p>
+            <p className="mt-2 mb-4 text-[#fcc133] italic mr-20">Tags: {meal.strTags}</p>
+            <p className="mt-4 mb-2 font-semibold text-lg mr-20 text-[#fcc133]">Instructions:</p>
+            <p className="mr-20 text-white text-justify">{meal.strInstructions}</p>
+            <p className="mt-4 mb-2 font-semibold text-lg mr-20">Ingredients:</p>
+            <table className="min-w-full mr-20 mb-5 border-collapse border border-white">
+                <thead>
+                    <tr className="text-gray-600">
+                        <th className="px-4 py-2 border-b border-r text-[#fcc133]">Items</th>
+                        <th className="px-4 py-2 border-b text-[#fcc133]">Quantity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {[...Array(20).keys()].map(i => {
+                        const ingredient = meal[`strIngredient${i + 1}`];
+                        const measure = meal[`strMeasure${i + 1}`];
+                        if (ingredient && ingredient.trim() !== "") {
+                            return (
+                                <tr key={i} className="text-white">
+                                    <td className="px-4 py-2 border-b border-r">{ingredient}</td>
+                                    <td className="px-4 py-2 border-b">{measure}</td>
+                                </tr>
+                            );
+                        }
+                        return null;  // Return null to satisfy React's map requirements
+                    })}
+                </tbody>
+            </table>
             {meal.strYoutube &&
             <button className="mt-4 ml-10 mr-20 mb-10 bg-orange-500 p-2 w-48 rounded-xl text-center hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500">
                 <a href={meal.strYoutube} target="_blank" rel="noreferrer" className="text-white">Watch Recipe Video</a>
